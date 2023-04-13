2d045ae763cf23d88fabdb60a671b2d1a645e107
Pablo <pabloarceproyects@gmail.com>
2023-04-14 00:21:25 +0200

    cambios varios

--- app/src/main/java/com/example/teammaker/MainActivity.kt
+++ app/src/main/java/com/example/teammaker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.teammaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val entryButton = findViewById<Button>(R.id.entry)
+        entryButton.setOnClickListener {
+            val intent = Intent(this, MenuOptions::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/teammaker/MenuOptions.kt
@@ -0,0 +1,11 @@
+package com.example.teammaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuOptions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menuoptions)
+    }
+}
